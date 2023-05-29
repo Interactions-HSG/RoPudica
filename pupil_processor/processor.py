56d1eea576bd5d661b816b84ee57b04e830f4597
Kay Jenss <44369294+kej-jay@users.noreply.github.com>
2023-05-29 14:37:27 +0200

    rework pupil to match left and right eye

--- pupil_processor/processor.py
+++ pupil_processor/processor.py
@@ -5,11 +5,14 @@ import json
 import time
 import datetime
 import csv
+import uuid
 
 EYE_INDEX_MAPPING = {
     0: 'right',
     1: 'left',
 }
+CONFIDENCE_TRHESHOLD = 0.6
+WRITE_TO_CSV = True
 
 # PUPILLABS CONNECTION DETAILS
 IP = 'localhost'
@@ -18,7 +21,8 @@ PORT = 50020
 # MQTT CONNECTION DETAILS
 BROKER="localhost"
 MQTT_PORT=1883
-DEBUG_MQTT = False
+MQTT_ACTIVE = False
+DEBUG_MQTT = True
 
 def on_publish(client,userdata,result):
     print("data published \n")
@@ -28,16 +32,17 @@ def approximate_timestamp(pupil_timestamp, offset):
     pupiltime_in_systemtime = pupil_timestamp + offset
     return datetime.datetime.fromtimestamp(pupiltime_in_systemtime)
 
-def write_to_csv(data):
-    with open('pupil.csv', mode='a', newline='') as f:
+def write_to_csv(data, filename='pupil.csv'):
+    with open(filename, mode='a', newline='') as f:
         csv_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         csv_writer.writerow(data.values())
 
 # MQTT
-client= mqtt.Client(protocol=mqtt.MQTTv5)
-if DEBUG_MQTT:
-    client.on_publish = on_publish
-client.connect(BROKER,MQTT_PORT)
+if MQTT_ACTIVE:
+    client= mqtt.Client(protocol=mqtt.MQTTv5)
+    if DEBUG_MQTT:
+        client.on_publish = on_publish
+    client.connect(BROKER,MQTT_PORT)
 
 # ZMQ
 ctx = zmq.Context()
@@ -55,26 +60,46 @@ sub_port = pupil_remote.recv_string()
 
 subscriber = ctx.socket(zmq.SUB)
 subscriber.connect(f'tcp://{IP}:{sub_port}')
-subscriber.subscribe('pupil.0.2d')  
-subscriber.subscribe('pupil.1.2d')  
+subscriber.subscribe('pupil.0.3d')  
+subscriber.subscribe('pupil.1.3d')
 
+last = {}
 while True:
     topic, payload = subscriber.recv_multipart()
     message = msgpack.loads(payload)
+    confidence = message[b'confidence']
+    
+    if confidence < CONFIDENCE_TRHESHOLD:
+        continue
     
     id = message[b'id']
     eye = EYE_INDEX_MAPPING[id]
-    diameter = message[b'diameter']
-    confidence = message[b'confidence']
+    diameter = message[b'diameter_3d']
+
     timestamp = approximate_timestamp(message[b'timestamp'], time_offset)
+
+    if last:
+        if last['eye'] == eye:
+            last = {
+                'eye': eye,
+                'diameter': diameter,
+            }
+            continue
+        else:
+            data = {
+                'id': str(uuid.uuid4()),
+                'diameters': (last['diameter'] + diameter)/2,
+                'timestamp': timestamp.isoformat(),
+            }
+    else:
+        last = {
+            'eye': eye,
+            'diameter': diameter,
+        }
+        continue
     
-    data = {
-        'id': id,
-        'eye': eye,
-        'diameter': diameter,
-        'confidence': confidence,
-        'timestamp': timestamp.isoformat(),
-    }
-    
-    write_to_csv(data)
-    # client.publish("debug/pupil/"+eye, json.dumps(data))
+    print(data)
+    if WRITE_TO_CSV:
+        write_to_csv(data)
+    if MQTT_ACTIVE:
+        client.publish("debug/pupil/"+eye, json.dumps(data))
